e600c84deef2018cc648bdf2342a944ae7e5773a
stk2k <stk2k@sazysoft.com>
2023-10-18 14:50:55 +0900

    first commit

---
+++ NeonDxLibApi_Sample_FullScreenMode/Program.cs
@@ -0,0 +1,33 @@
+﻿using NeonDX.DxLibApi;
+
+namespace NeonDxLibApi_Sample_FullScreenMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DXライブラリの初期化
+            NDX_API_System.DxLib_Init();
+
+            while (true)
+            {
+                // 裏画面をクリア
+                NDX_API_Graphics2D.ClearDrawScreen();
+
+                // ESCキーが押されていたら終了
+                if (NDX_API_Input.CheckHitKey(NDX_API_Input.KEY_INPUT_ESCAPE))
+                {
+                    break;
+                }
+
+                // 描画処理をここに書きます
+
+                // 裏画面に反映
+                NDX_API_Graphics2D.ScreenFlip();
+            }
+
+            // DXライブラリの終了
+            NDX_API_System.DxLib_End();
+        }
+    }
+}
---
+++ NeonDxLibApi_Sample_WindowMode/Program.cs
@@ -0,0 +1,45 @@
+﻿using NeonDX.DxLibApi;
+
+namespace NeonDxLibApi_Sample_WindowMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ウィンドウモード
+            NDX_API_Window.ChangeWindowMode(true);
+
+            // Alt+F4やウィンドウの閉じるボタンを無効化
+            NDX_API_Window.SetWindowUserCloseEnableFlag(false);
+
+            // DXライブラリの初期化
+            NDX_API_System.DxLib_Init();
+
+            while (true)
+            {
+                // 裏画面をクリア
+                NDX_API_Graphics2D.ClearDrawScreen();
+
+                // ESCキーが押されていたら終了
+                if (NDX_API_Input.CheckHitKey(NDX_API_Input.KEY_INPUT_ESCAPE))
+                {
+                    break;
+                }
+
+                // Alt+F4や閉じるボタンが押されていたら終了
+                if (NDX_API_Window.GetWindowUserCloseFlag(true))
+                {
+                    break;
+                }
+
+                // 描画処理をここに書きます
+
+                // 裏画面に反映
+                NDX_API_Graphics2D.ScreenFlip();
+            }
+
+            // DXライブラリの終了
+            NDX_API_System.DxLib_End();
+        }
+    }
+}
